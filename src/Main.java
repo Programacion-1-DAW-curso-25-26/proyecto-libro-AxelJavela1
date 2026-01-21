b724edfda969f9e1e0da03f84c0fb9df2acf0cc0
AxelJavela1 <Axejavota@alu.edu.gva.es>
2026-01-21 13:01:07 +0100

    Clase Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        Libro libro1 = new Libro("Cien años de soledad", "Gabriel García Márquez", 471, 9, 12345);
+        Libro libro2 = new Libro("El Quijote", "Miguel de Cervantes", 863, 10, 67890);
+        Libro libro3 = new Libro("Cien años de soledad", "Gabriel García Márquez", 471, 9, 12345);
+
+
+        System.out.println("Información del libro 1:");
+        libro1.mostarInfo();
+
+        System.out.println("\nInformación del libro 2:");
+        libro2.mostarInfo();
+
+
+        System.out.println("\n¿El libro 1 es igual al libro 2? " + libro1.equals(libro2));
+        System.out.println("¿El libro 1 es igual al libro 3? " + libro1.equals(libro3));
+
+
+        System.out.println("\nUsando toString():");
+        System.out.println(libro1);
+
+
+        libro2.setValoracion(8);
+        libro2.setNumpaginas(900);
+
+        System.out.println("\nLibro 2 después de modificar datos:");
+        libro2.mostarInfo();
     }
 }
